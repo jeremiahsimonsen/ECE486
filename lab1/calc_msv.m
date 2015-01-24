2221b8643b8a26d189ac05621b836ddf6684c633
Jeremiah Simonsen <jeremiah.simonsen@maine.edu>
2015-01-24 15:51:12 -0500

    Initial commit

---
+++ lab1/calc_msv.m
@@ -0,0 +1,74 @@
+function [y,s] = calc_msv(x,s)
+%CALC_MSV Calculates the "running mean-square value" of a sample vector
+%	[y,s] = calc_msv(x,s) calculates the running mean square value(msv) of
+%	the sample vector 'x', storing the msv in vector 'y'. Information for
+%	the calculation is stored in the structure 's', such as the number of
+%	samples to average over, 'M', the last M-1 samples in any previous
+%	blocks of 'blocksize' samples are stored in 'history', the current
+%	index of history is stored in 'histIndex', the number of samples in
+%	history is stored in 'samples', and a running sum of the squares of the
+%	previous M-1 samples is stored in 'prevSquared'.
+%
+%	Example: If s = 
+%					M: 3, 
+%					blocksize: 7,
+%					prevSquared: 0, 
+%					history: [0
+%							  0]
+% 					samples: 0
+% 					histIndex: 1
+%	and x = [1 2 3 4 5 6 7]' then
+%					[y,s] = calc_msv(x,s)
+%				returns y = [1
+%							 2.5
+% 							 4.6667
+% 							 9.6667
+% 							 16.6667
+% 							 25.6667
+% 							 36.6667]
+% 						
+% 						s = 
+% 							M: 3,
+% 							blocksize: 7,
+% 							prevSquared: 85,
+% 							history: [7
+% 									  6]
+% 							samples: 2
+% 							histIndex: 2. 
+
+% Authors: Jacob Allenwood, Travis Russell, Jeremiah Simonsen
+% ECE 486 - Laboratory 1
+% January 22, 2015
+
+x = x(:);	% Force the input sample vector to be a column vector
+
+for n=1:size(x,1) % index through the input array
+	% Test if the history buffer is full or not and calculate y(n)
+	% accordingly
+	if (s.samples < (s.M - 1))									% buffer not full
+		y(n,1) = (s.prevSquared + x(n)^2) / (s.samples + 1);	% average over #samples we have
+	else														% buffer full
+		y(n,1) = (s.prevSquared + x(n)^2) / s.M;				% average over M samples
+	end
+	
+	s.prevSquared = s.prevSquared - s.history(s.histIndex)^2;	% subtract (the oldest value)^2
+	s.prevSquared = s.prevSquared + x(n)^2;						% add (the new value)^2
+	s.history(s.histIndex) = x(n);								% update value in history buffer
+	s.histIndex = s.histIndex + 1;								% next spot in the buffer is now the oldest
+	
+	% Handle looping back around to the beginning of the history buffer.
+	% Avoids using modulus just in case it is less efficient on the
+	% processor
+	if (s.histIndex == s.M)										% reached the end
+		s.histIndex = s.histIndex - (s.M - 1);					% go to beginning
+	end
+	
+	% Only increment number of samples in history buffer if it is not full.
+	% This prevents the possibility of the number looping back around to
+	% its minimum
+	if (s.samples < s.M)
+		s.samples = s.samples + 1;
+	end
+end
+
+end
---
+++ lab1/init_msv.m
@@ -0,0 +1,36 @@
+function s = init_msv(M,blocksize)
+%INIT_MSV Initializes structure for running mean-square value 
+%   s = init_msv(M, blocksize) initializes a structure containing all
+%   necessary fields to compute the running mean squared value of a block
+%   of samples 'blocksize' in length, over the most recent 'M' samples. The
+%   field 'prevSquared' is a running sum of the squares of the most recent
+%   M samples. 'history' is a circular buffer containing the last M-1
+%   elements of the sample, necessary in calculating the running mean
+%   squared value for subsequent calls of the calc_msv() function.
+%   'samples' is the number of samples contained in history. 'histIndex' is
+%   the current location in the circular history buffer.
+%
+%   Example: If M = 4 and blocksize = 10 then
+%                s = init_msv(4,10) 
+%           returns s = 
+%				M: 4
+%				blocksize: 10
+%				prevSquared: 0
+%				history: [0 
+%						  0 
+%						  0].
+%				samples: 0
+% 				histIndex: 1
+
+% Authors: Jacob Allenwood, Travis Russell, Jeremiah Simonsen
+% ECE 486 - Laboratory 1
+% January 21, 2015
+
+s = struct( 'M',M, ...
+			'blocksize',blocksize, ...
+			'prevSquared', 0, ...
+			'history', zeros(M-1,1), ...
+			'samples', 0, ...
+			'histIndex',1);
+
+end
