a8c75e7d5df827c9f7e62cf467c10884d0a52b14
Travis Russell <travis.russell@maine.edu>
2015-01-24 20:14:19 -0500

    Added doxygen header

---
+++ lab1/msv_subs.h
@@ -0,0 +1,16 @@
+/*!
+ * @file
+ * 
+ * @brief ECE 486 Spring 2014 Lab 1 Subroutines
+ * 
+ * @author ECE486 Lab Group 2
+ * @author Jacob Allenwood, Travis Russell, Jeremiah Simonsen
+ * 
+ * @date Jan 24, 2015
+ * 
+ * This is an example "include" file which actually only contains
+ * the single prototype for the add_10() function.  If this include
+ * file were to support a more complex library of functions, I'd be
+ * describing the use of the library.
+ * 
+ */
