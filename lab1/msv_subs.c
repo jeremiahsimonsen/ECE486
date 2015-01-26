ad7316c2abe84eb9f1f766a21da0efd61511c6f0
jeremiah.simonsen <jeremiah.simonsen@maine.edu>
2015-01-26 14:37:40 -0500

    Added Makefile. Added additional comments to source files.

--- lab1/msv_subs.c
+++ lab1/msv_subs.c
@@ -1,3 +1,29 @@
+/*!
+ * @file
+ * 
+ * @brief ECE 486 Spring 2015 Lab 1 Subroutine Definitions
+ * 
+ * @author ECE486 Lab Group 2
+ * @author Jacob Allenwood, Travis Russell, Jeremiah Simonsen
+ * 
+ * @date Jan 26, 2015
+ * 
+ * This file contains the subroutine definitions necessary for calculating the 
+ * running mean-square-values of a block of samples.
+ *
+ * The init_msv() function initializes a struct of type MSV_STRUCT_T. It stores
+ * the number of samples to average over 'M', the number of samples in a block
+ * 'blocksize', and a circular buffer of past samples 'history'. It returns a
+ * pointer to said struct.
+ *
+ * The calc_msv() function performs the running mean-square-value calculations
+ * on a block of samples. It takes a float pointer 'x' to indicate the array of 
+ * input samples and a MSV_STRUCT_T pointer 's' to indicate the struct with the 
+ * information necessary to perform the calcuations. It returns a float pointer
+ * to indicate the array of output running mean-square-values.
+ * 
+ */
+
 #include "msv_subs.h"
 #include <stdio.h>
 
@@ -13,5 +39,7 @@ MSV_STRUCT_T * init_msv(uint M, uint blocksize){
 	for(i=0;i<M;i++){
 		s.history[i] = 0;
 	}
+
+  
 
 }
--- lab1/msv_subs.h
+++ lab1/msv_subs.h
@@ -1,13 +1,30 @@
 /*!
  * @file
  * 
- * @brief ECE 486 Spring 2014 Lab 1 Subroutines
+ * @brief ECE 486 Spring 2015 Lab 1 Subroutine Header file
  * 
  * @author ECE486 Lab Group 2
  * @author Jacob Allenwood, Travis Russell, Jeremiah Simonsen
  * 
  * @date Jan 24, 2015
  * 
+ * This file contains subroutine and data-type declarations necessary for 
+ * running mean-square-value calculations.
+ * 
+ * The MSV_STRUCT_T data-type is a struct containing the necessary fields for
+ * calculating the running mean-square-value.
+ *
+ * The init_msv() function initializes an MSV_STRUCT_T variable, allocating
+ * memory for all fields. The function takes an integer 'M' indicating the
+ * number of samples to average over and an integer 'blocksize' indicating the
+ * number of samples in each block of data to be processed. The function returns
+ * a pointer to an MSV_STRUCT_T.
+ *
+ * The calc_msv() function performs the running mean-square-value calculation on
+ * a block of samples. It takes a pointer to a float 'x' - the block of samples-
+ * and a pointer to an MSV_STRUCT_T 's' - the struct containing the fields 
+ * necessary for the calculations. The function returns a pointer to an float,
+ * the array of running mean-square-values.
  * 
  */
 
@@ -32,25 +49,27 @@ typedef struct msv_struct {
  * @brief Initializes an MSV_STRUCT_T structure.
  *
  * @returns A pointer to a structure of type MSV_STRUCT_T is returned
- * 			containing the fields necessary for a mean squared value
- * 			calculation of 'blocksize' samples, averaged over 'M'
- *			samples.
+ * 			    containing the fields necessary for a mean squared value
+ * 			    calculation of @a blocksize samples, averaged over @a M
+ *			    samples.
  */
 
 MSV_STRUCT_T * init_msv(
-	uint M, 				/*!< [in] The number of samples to average over */
-	uint blocksize			/*!< [in] The number of samples in each block */
+	uint M, 				   /*!< [in] The number of samples to average over */
+	uint blocksize		 /*!< [in] The number of samples in each block */
 );
 
 /*!
  * @brief Calculates the mean squared value of a block of samples.
  * 
- * @returns On return, an array of output samples 
+ * @returns A pointer to an array of running mean-square-values calculated from
+ *          the block of samples @a x.
  */
 
 float * calc_msv(
-	float * x, 
-	MSV_STRUCT_T * s
+	float * x,         //!< [in] Pointer to an array of floats to be operated upon
+	MSV_STRUCT_T * s   /*!< [in] Pointer to an MSV_STRUCT_T containing additional
+                          fields necessary for the calculation */
 );
 
 #endif
