79426131c2fd018efa48419ab87e8f1f20c5a40d
= <jeremiah.simonsen@maine.edu>
2015-01-30 15:48:37 -0500

    Added m=blocksize testcase

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -34,6 +34,48 @@
  */
 
 int main(int argc, char *argv[]) {
+  // Test when M < blocksize
+  float x1[7] = {1,2,3,4,5,6,7};      // Initialize sample array of blocksize 7
+  MSV_STRUCT_T *s1 = init_msv(3,7);   // Initialize struct for storing calculation info
+  float *y1 = calc_msv(x1,s1);          // Calculate the mean-square value and store in y
+
+  // Display output
+  // Should match: 
+  // 0.333333 1.666667 4.666667 9.666667 16.666666 25.666666 36.666668
+  int i;
+  for (i = 0; i < 7; i++)  {
+      printf("%f ", y1[i]);
+  }
+  printf("\n\n");
+
+  /********************************************************************/
+  // Test when M = blocksize
+  // Should match:
+  // 0.333333 1.666667 4.666667 
+  // 9.666667 16.666666 25.666666 
+  // 36.666668 49.666668 64.666664
+  /********************************************************************/
+
+  float x2[3] = {1,2,3};
+  float x3[3] = {4,5,6};
+  float x4[3] = {7,8,9};
+  MSV_STRUCT_T *s2 = init_msv(3,3);
+  float *y2 = calc_msv(x2,s2);
+  float *y3 = calc_msv(x3,s2);
+  float *y4 = calc_msv(x4,s2);
+  for(i=0; i<3; i++) {
+    printf("%f ", y2[i]);
+  }
+  printf("\n");
+  for(i=0; i<3; i++) {
+    printf("%f ", y3[i]);
+  }
+  printf("\n");
+  for(i=0; i<3; i++) {
+    printf("%f ", y4[i]);
+  }
+  printf("\n");
+  
 
   return 0;
 }
