496735a35c36cb88b72786e366f3d3f5997c25d1
Jeremiah Simonsen <jeremiah.simonsen@maine.edu>
2015-03-10 22:20:19 -0400

    Added shading

--- lab3/task2.m
+++ lab3/task2.m
@@ -3,12 +3,13 @@
 % 0.25 and attenuate frequencies f<=0.05 and f>=0.32 by at least 35 dB
 
 %% Generate filter coefficients
-pf = [0.15 0.25];			% pole frequencies
+pf = [0.1 0.12 0.2 0.25];			% pole frequencies
 bw = (0.25 - 0.1)*2*pi;		% bandwidth
 r = 1 - bw/(2*length(pf));	% radius of poles
-zf = [0 0.5];				% zero frequencies
+% r = .1;
+zf = [0 .02 .05 0.33 0.5];				% zero frequencies
 p = [r*exp(1i*2*pi.*pf) r*exp(-1i*2*pi.*pf)];	% poles
-z = [1*exp(1i*2*pi.*zf) 1*exp(-1i*2*pi.*zf)];	% zeros
+z = [1*exp(1i*2*pi.*zf)];						% zeros
 a = poly(p);
 b = poly(z);
 
@@ -29,7 +30,10 @@ G1 = num ./ den;
 K = 1/max(abs(G1));
 G1 = K .* G1;
 
-figure(1);
+figure(1); hold on;
+patch([0.1 0.1 0.25 0.25],[-7 -5 0 -2],'g');
+patch([0 0 0.05 0.05],[-45 -40 -40 -45],'r');
+patch([0.32 0.32 0.5 0.5],[-45 -35 -35 -45],'r');
 plot(f,20*log10(abs(G1)));
 axis([0 0.5 -45 5]);
 xlabel('Frequency (cycles/sample)'); ylabel('Filter Gain (dB)');
