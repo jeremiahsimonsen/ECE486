672995243ae4af28e9eda6e52ad65acae86caf1a
Jeremiah Simonsen <jeremiah.simonsen@maine.edu>
2015-03-11 23:09:14 -0400

    Now outputs header file and c source file defining filter coefficients automatically. Just need to modify the test code to use these instead.

---
+++ lab3/biquad_coef.c
@@ -0,0 +1,4 @@
+#include "biquad_coef.h"
+float b_coef[1][3] = {{1, -2, 1}};
+float a_coef[1][3] = {{1, -2, 1}};
+float g = 0.284153;
---
+++ lab3/biquad_coef.h
@@ -0,0 +1,3 @@
+extern float a_coef[][3];
+extern float b_coef[][3];
+extern float g;
