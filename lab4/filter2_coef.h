6b2953042c9e253d9373ae63bf70705a51c84b36
= <jeremiah.simonsen@maine.edu>
2015-03-22 18:53:04 -0400

    Updated comments

--- lab4/filter2_coef.h
+++ lab4/filter2_coef.h
@@ -1,16 +1,16 @@
 /*!
  * @file
  * 
- * @brief ECE 486 Spring 2015 Lab 3 - Header file declaring externally
+ * @brief ECE 486 Spring 2015 Lab 4 - Header file declaring externally
  * defined filter coefficients.
  * 
  * @author ECE486 Lab Group 2
  * @author Jacob Allenwood, Travis Russell, Jeremiah Simonsen
  * 
- * @date Mar 10, 2015
+ * @date Mar 22, 2015
  * 
  * This file contains the declaration of the filter coefficients that will
- * defined in biquad_coef.c The coefficients will be the output of the 
+ * defined in filter2_coef.c The coefficients will be the output of the 
  * zp2biquad() MATLAB function. This implementation allows for the implementation
  * of a new filter by calling the zp2biquad() function with the new poles
  * and zeros and then re-compiling.
