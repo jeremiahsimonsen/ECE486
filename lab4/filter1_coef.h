f1a53182ec305a31c721039537fb82316f060b48
= <jeremiah.simonsen@maine.edu>
2015-03-22 18:51:36 -0400

    Filter coefficient headers

---
+++ lab4/filter1_coef.h
@@ -0,0 +1,26 @@
+/*!
+ * @file
+ * 
+ * @brief ECE 486 Spring 2015 Lab 3 - Header file declaring externally
+ * defined filter coefficients.
+ * 
+ * @author ECE486 Lab Group 2
+ * @author Jacob Allenwood, Travis Russell, Jeremiah Simonsen
+ * 
+ * @date Mar 10, 2015
+ * 
+ * This file contains the declaration of the filter coefficients that will
+ * defined in biquad_coef.c The coefficients will be the output of the 
+ * zp2biquad() MATLAB function. This implementation allows for the implementation
+ * of a new filter by calling the zp2biquad() function with the new poles
+ * and zeros and then re-compiling.
+ *
+ * This header file must be included by any program that uses the filter
+ * coefficients output by the zp2biquad() function.
+ * 
+ */
+
+extern float filter1_a_coef[][3];
+extern float filter1_b_coef[][3];
+extern float filter1_g;
+extern int filter1_num_stages;
---
+++ lab4/filter2_coef.h
@@ -0,0 +1,26 @@
+/*!
+ * @file
+ * 
+ * @brief ECE 486 Spring 2015 Lab 3 - Header file declaring externally
+ * defined filter coefficients.
+ * 
+ * @author ECE486 Lab Group 2
+ * @author Jacob Allenwood, Travis Russell, Jeremiah Simonsen
+ * 
+ * @date Mar 10, 2015
+ * 
+ * This file contains the declaration of the filter coefficients that will
+ * defined in biquad_coef.c The coefficients will be the output of the 
+ * zp2biquad() MATLAB function. This implementation allows for the implementation
+ * of a new filter by calling the zp2biquad() function with the new poles
+ * and zeros and then re-compiling.
+ *
+ * This header file must be included by any program that uses the filter
+ * coefficients output by the zp2biquad() function.
+ * 
+ */
+
+extern float filter2_a_coef[][3];
+extern float filter2_b_coef[][3];
+extern float filter2_g;
+extern int filter2_num_stages;
