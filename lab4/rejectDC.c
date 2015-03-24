083cd1c5a39d97a05e350b51913242672a29b38a
= <jeremiah.simonsen@maine.edu>
2015-03-23 22:27:22 -0400

    Added DC rejection routine. High frequency messed up

---
+++ lab4/rejectDC.c
@@ -0,0 +1,51 @@
+/**
+ * @file
+ * 
+ * @brief ECE 486 Spring 2015 DC rejection routine source file
+ * 
+ * @author ECE486 Lab Group 2
+ * @author Jacob Allenwood, Travis Russell, Jeremiah Simonsen
+ * 
+ * @date Mar 23, 2015
+ * 
+ * This file contains subroutine definitions necessary for
+ * running the DC rejection filter routine.
+ */
+
+ #include "rejectDC.h"
+ #include <stdlib.h>
+
+
+DCBLOCK_T *init_dcblock(int bSize) {
+
+	// Allocate memory for DCBLOCK_T structure
+	DCBLOCK_T *s;
+	s = (DCBLOCK_T *)malloc(sizeof(DCBLOCK_T));
+	if (s == NULL) return NULL;
+
+	// initialize variables
+	s->blockSize = bSize;
+	s->x_prev = 0.0;
+	s->y_prev = 0.0;
+
+	return s;
+}
+
+
+void calc_dcblock(DCBLOCK_T *s,	float *x, float *y) {
+
+	int n;
+	for (n = 0; n < s->blockSize; n++) {
+		if (n == 0) {
+			y[n] = 0.99*s->y_prev + 0.995*(x[n] - s->x_prev);
+		} else {
+			y[n] = 0.99*y[n-1] + 0.995*(x[n] - x[n-1]);
+		}
+	}
+
+}
+
+
+void destroy_dcblock(
+	DCBLOCK_T *s 			/*!< Pointer to a DCBLOCK_T to be destroyed */
+);
---
+++ lab4/rejectDC.h
@@ -0,0 +1,61 @@
+/**
+ * @file
+ * 
+ * @brief ECE 486 Spring 2015 DC rejection routine header file
+ * 
+ * @author ECE486 Lab Group 2
+ * @author Jacob Allenwood, Travis Russell, Jeremiah Simonsen
+ * 
+ * @date Mar 23, 2015
+ * 
+ * This file contains subroutine and data-type declarations necessary for
+ * running the DC rejection filter routine.
+ */
+
+#ifndef REJECTDC_H
+#define REJECTDC_H
+
+/**
+ * @brief Structure for FIR DC rejection filter implementation
+ * @details This structure contains the number of samples per block,
+ * 'blockSize', and the state information - previous values of x(n) and y(n)
+ * 
+ */
+typedef struct dc_block_struct {
+
+	int blockSize;			/*!< Number of samples to process */
+	float x_prev;			/*!< Previous value of input; x(n-1) */
+	float y_prev;			/*!< Previous value of output; y(n-1) */
+
+} DCBLOCK_T;
+
+/**
+ * @brief Initializes a DCBLOCK_T structure
+ * @details Initializes variables required for DC blocking filter
+ * @returns A pointer to a structure of type DCBLOCK_T containing the fields
+ * necessary for DC blocking filter implementation
+ */
+DCBLOCK_T *init_dcblock(	
+	int bSize				/*!< Number of samples to process */
+);
+
+/**
+ * @brief Performs DC blocking filter calculation on a block of samples
+ * @details An input sample array, @x, is filtered to reject DC and the filter
+ * output is sent to output sample array, @y.
+ * @returns Filter output - input signal with DC rejected
+ */
+void calc_dcblock(
+	DCBLOCK_T *s,			/*!< Structure containing necessary filter info */
+	float *x,				/*!< Array of input samples */
+	float *y 				/*!< Array of outputs */
+);
+
+/**
+ * @brief De-allocates the memory required for a DCBLOCK_T
+ */
+void destroy_dcblock(
+	DCBLOCK_T *s 			/*!< Pointer to a DCBLOCK_T to be destroyed */
+);
+
+#endif
