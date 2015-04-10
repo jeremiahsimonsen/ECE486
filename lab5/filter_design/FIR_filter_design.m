6e70093bd6bfb4d9b68e7096807957c4d05c9514
Travis Russell <travis.russell@maine.edu>
2015-04-10 02:03:30 -0400

    Reduced order of FIR filter to 75

--- lab5/filter_design/FIR_filter_design.m
+++ lab5/filter_design/FIR_filter_design.m
@@ -7,18 +7,18 @@
 %%
 N = 16*1024;
 Fs = 48e3;
-M = 81;
+M = 75;
 % f = (0:N-1)/N;
 % f(N/2+2:end) = f(N/2+2:end)-1;
 f = linspace(-0.5,0.5,N);
 F = f*Fs;
-Hr = (abs(f)<.095);
+Hr = (abs(f)<.0955)*1.01;
 hr = ifft(Hr);
 figure(1); clf;
 stem(0:N-1,abs(hr),'.');
 Hd = Hr.*exp(-1i*2*pi*f*(M-1)/2);
 hd = real(ifft(Hd));
-h = hd(1:M).*kaiser(M,7.8573)';
+h = hd(1:M).*kaiser(M,8)';
 hold on;
 stem(1:M,h,'r.');
 figure(2); clf; hold on;
