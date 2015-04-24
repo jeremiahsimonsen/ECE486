d1b5c1dae6978755c097abea53daaf673bd63bee
Jacob Allenwood <jacob.allenwood@maine.edu>
2015-04-24 14:00:24 -0400

    clean up and comment real_stepped

--- lab6/radar/real_time_stepped.c
+++ lab6/radar/real_time_stepped.c
@@ -1,45 +1,37 @@
 /*!
  * @file
  * 
- * @brief ECE 486 Spring 2015 Lab 5 DSP for a doppler radar detector
+ * @brief ECE 486 Spring 2015 Lab 6 DSP for a stepped frequency radar detector
  * 
  * @author ECE486 Lab Group 2
  * @author Jacob Allenwood, Travis Russell, Jeremiah Simonsen
  * 
- * @date Apr 10, 2015
+ * @date Apr 24, 2015
  * 
  * This file contains the real-time implementation of the digital signal
- * processing for a doppler radar detector.
+ * processing for a stepped frequency radar detector.
  *
- * A 5.8 GHz signal is transmitted from one antenna. The signal reflects off of
+ * The transmit frequency is approximately 5.8 GHz, but may be adjusted by changing 
+ * the VCO control voltage (about 75 MHz/V over the zero to five volt range). 
+ * We use the DAC output to create voltages between one and three volts, so that our measured radar returns will 
+ * be collected over a 150 MHz band. The key to the stepped frequency radar is that the VCO is used to increment 
+ * the transmitter frequency to provide multiple measurement taken at equally spaced frequencies.The transmitted signal reflects off of
  * objects. Moving objects cause a change in frequency known as Doppler shift,
  * roughly proportional to their velocity. The reflected signal is received by
- * a second antenna and split into two signals 90 degrees out of phase. Each
- * signal is mixed with the transmitted signal to find the difference frequency, which corresponds to the Doppler shift. 
- * The mixed signals are then low-passed filtered and sampled by the STM32F407 development board.
+ * a second antenna and split into two signals 90 degrees out of phase. 
+ * By estimating the discrete-time frequency of the sequence of measurements, the distance to the target may be obtained. 
+ * Of course, the received signal normally consists of many reflections off of multiple targets, and each target provides 
+ * its own contribution to the sequence x(n). An FFT is used to examine the received sequence x(n) and determine what frequencies 
+ * (distances) are present, and what the signal strength is for the reflection from each distance. \
  * 
- * The two ADC inputs are sampled at 24 ksps. The signals are lowpass filtered
- * to limit the bandwidth to 2 kHz (equivalent to about 50 m/s). The sampled
- * signals are decimated to reduce the necessary processing time. An FFT is
- * performed to examine the spectrum of the complex input signal. The highest
- * positive and negative velocities are detected by taking the highest positive
- * and negative frequencies with FFT component above a certain threshold. These
- * velocities are then written to the serial port, so they can be read.
  * 
  */
 
-// Define the decimation rate...
 #define MY_FS 12000.0
-// #define D1 4
-// #define D2 4
 #define MY_NSAMP 256
-// #define BLOCKS 16
 #define FFT_N 512
 #define FORWARD_FFT 0
 #define INVERSE_FFT 1
-
-// #define POS_THRESHOLD 10.0
-// #define NEG_THRESHOLD 10.0
 
 
 #include "stm32f4xx_hal.h"
@@ -57,7 +49,6 @@
 #include <stdlib.h>
 #include <math.h>
 #include "arm_math.h"
-// #include "arm_common_tables.h"
 
 // #include "ece486_fir.h"			// FIR filter routines
 #include "ece486_biquad.h"			// IIR filter routines
@@ -67,9 +58,8 @@
 #include "numtostr.h"
 #include "ece486_mixer.h"
 
-int main(void)
+int main(void) 
 {
-	// static int block_count = 0;
 
 	// Set the ADC/DAC Block size; note the code below assumes MY_NSAMP is a
 	// multiple of D1
@@ -88,10 +78,6 @@ int main(void)
  	// DAC outputs
  	// float *output1 = (float *)malloc(sizeof(float)*MY_NSAMP);
  	float *output2 = (float *)malloc(sizeof(float)*MY_NSAMP);
-
- 	// Decimated real and imaginary signals; outputs of lowpass filter
- 	// float32_t *buffer_re = (float32_t *)malloc(sizeof(float32_t)*MY_NSAMP/D1);
- 	// float32_t *buffer_im = (float32_t *)malloc(sizeof(float32_t)*MY_NSAMP/D1);
 
  	// Input (and output) to the complex FFT; interleaved of above and zero pad
  	float32_t *fft_in = (float32_t *)malloc(sizeof(float32_t)*FFT_N*2);
@@ -115,10 +101,6 @@ int main(void)
  		while(1);
  	}
   
- 	// Filter initializations
-	// BIQUAD_T *low1_re = init_biquad(lowpass_num_stages, lowpass_g, lowpass_a_coef, lowpass_b_coef, MY_NSAMP);
-	// BIQUAD_T *low1_im = init_biquad(lowpass_num_stages, lowpass_g, lowpass_a_coef, lowpass_b_coef, MY_NSAMP);
-
 	float ramp[256] = {-0.3333,
 		   -0.3281,
 		   -0.3229,
@@ -648,16 +630,6 @@ int main(void)
     	 */
 		DIGITAL_IO_SET();
 
-    	// Lowpass filter
-		// calc_biquad(low1_re,input1,input1);
-		// calc_biquad(low1_im,input2,input2);
-
-    	// Decimate by D1
-    	// for (i=0; i<MY_NSAMP/D1; i++) {
-    	// 	buffer_re[i] = input1[i*D1];
-    	// 	buffer_im[i] = input2[i*D1];
-    	// }
-
     	// Windowing here may be helpful to discern between frequencies if the velocities are close together
 	    calc_mixer(window_mix, input1, input1);	// OK to use same mixer b/c
 	    calc_mixer(window_mix, input2, input2);	// input1 & kaiser same size
@@ -667,36 +639,18 @@ int main(void)
     		fft_in[2*i] = input1[i];
     		fft_in[2*i+1] = input2[i];
     	}
-    	// block_count++;
 
-    	// if (block_count == D2) {
-    		// block_count = 0;
+    	// Zero the rest of the fft_in array
+    	for (i=2*MY_NSAMP; i<2*FFT_N; i++) {
+    		fft_in[i] = 0.0;
+    	}
 
-	    	// Zero the rest of the fft_in array
-	    	for (i=2*MY_NSAMP; i<2*FFT_N; i++) {
-	    		fft_in[i] = 0.0;
-	    	}
+    	// Calculate complex fft
+    	arm_cfft_radix2_f32(&fft, fft_in);
 
-	    	// Calculate complex fft
-	    	arm_cfft_radix2_f32(&fft, fft_in);
-
-	    	// Calculate complex magnitude
-	    	arm_cmplx_mag_f32(fft_in, mag, FFT_N);
-	    
-	    	/* 
-	    	 * Write output values to the DAC....  NOTE: Be sure to set the output
-	    	 * array to values for every INPUT sample (not just samples at the decimated 
-	    	* rates!
-	    	*/
-	    // 	for (i=0; i<MY_NSAMP/D1; i++) {
-	    // 	  	// Every stage-3 output should be written to D1 output samples!
-	    // 	  	for (j=0; j<D1; j++) {
-					// output1[i*D1+j] = buffer[i];
-	    // 		}
-	    // 	}
-	    
-    	// }
-
+    	// Calculate complex magnitude
+    	arm_cmplx_mag_f32(fft_in, mag, FFT_N);
+    
 	    arm_max_f32(&mag[20],FFT_N/2-20,&max_fft,&max_ind);
 
 	    for (i=0; i<MY_NSAMP; i++) {
