2ddbeca5459d478c3fab207c801c18a6ca99fb83
= <jeremiah.simonsen@maine.edu>
2016-06-25 13:27:53 -0400

    Unknown change

--- lab3/ece486_biquad_test.c
+++ lab3/ece486_biquad_test.c
@@ -39,54 +39,56 @@
 
 int main(void)
 {
-	int nsamp, i;
- 	float *input, *output1, *output2;
+	// int nsamp, i;
+ // 	float *input, *output1, *output2;
  	initialize(FS_48K, MONO_IN, STEREO_OUT); 	// Set up the DAC/ADC interface
- 	
+ 	while(1){
+    UART_putstr("Hello");
+}
  	// Allocate Required Memory
- 	nsamp = getblocksize();
+ 	// nsamp = getblocksize();
 	
- 	input = (float *)malloc(sizeof(float)*nsamp);
- 	output1 = (float *)malloc(sizeof(float)*nsamp);
- 	output2 = (float *)malloc(sizeof(float)*nsamp);
+ 	// input = (float *)malloc(sizeof(float)*nsamp);
+ 	// output1 = (float *)malloc(sizeof(float)*nsamp);
+ 	// output2 = (float *)malloc(sizeof(float)*nsamp);
   
- 	if (input==NULL || output1==NULL || output2==NULL) {
- 		flagerror(MEMORY_ALLOCATION_ERROR);
- 		while(1);
- 	}
+ 	// if (input==NULL || output1==NULL || output2==NULL) {
+ 	// 	flagerror(MEMORY_ALLOCATION_ERROR);
+ 	// 	while(1);
+ 	// }
 
  	// Filter coefficients
- 	float a[2][3] = {{1.0, -0.6088, 0.9702},
-           {1.0, -0.3500, -0.4250}};
+ // 	float a[2][3] = {{1.0, -0.6088, 0.9702},
+ //           {1.0, -0.3500, -0.4250}};
 
-  	float b[2][3] = {{2.2044, 0.0, 2.2044},
-           {2.9658, -3.4865, 2.9658}};
+ //  	float b[2][3] = {{2.2044, 0.0, 2.2044},
+ //           {2.9658, -3.4865, 2.9658}};
 
-    // Biquad structure initialization
-	BIQUAD_T *f1;
-	f1 = init_biquad(2, 0.01718740, a, b, nsamp);
+ //    // Biquad structure initialization
+	// BIQUAD_T *f1;
+	// f1 = init_biquad(2, 0.01718740, a, b, nsamp);
 
-	// Infinite Loop to process the data stream, "nsamp" samples at a time	
-	while(1){
-    	/*
-    	 * Ask for a block of ADC samples to be put into the working buffer
-    	 *   getblock() will wait until the input buffer is filled...  On return
-    	 *   we work on 	the new data buffer.
-    	 */
-    	getblock(input);	// Wait here until the input buffer is filled... Then process	
+	// // Infinite Loop to process the data stream, "nsamp" samples at a time	
+	// while(1){
+ //    	/*
+ //    	 * Ask for a block of ADC samples to be put into the working buffer
+ //    	 *   getblock() will wait until the input buffer is filled...  On return
+ //    	 *   we work on 	the new data buffer.
+ //    	 */
+ //    	getblock(input);	// Wait here until the input buffer is filled... Then process	
     	
-    	// signal processing code to calculate the required output buffers
-    	// copy input to output2 for reference
-    	for(i=0;i<nsamp;i++) {
-    		output2[i] = input[i];
-    	}
-    	DIGITAL_IO_SET(); 	// Use a scope on PC4 to measure execution time
-    	// Call the biquad filter routine
-    	calc_biquad(f1,input,output1);
+ //    	// signal processing code to calculate the required output buffers
+ //    	// copy input to output2 for reference
+ //    	for(i=0;i<nsamp;i++) {
+ //    		output2[i] = input[i];
+ //    	}
+ //    	DIGITAL_IO_SET(); 	// Use a scope on PC4 to measure execution time
+ //    	// Call the biquad filter routine
+ //    	calc_biquad(f1,input,output1);
     	
-    	DIGITAL_IO_RESET();	// (falling edge....  done processing data )
+ //    	DIGITAL_IO_RESET();	// (falling edge....  done processing data )
     	
-    	// pass the processed working buffer back for DAC output
-    	putblockstereo(output1, output2);
-	}
+ //    	// pass the processed working buffer back for DAC output
+ //    	putblockstereo(output1, output2);
+	// }
 }
